1610bbafb77f58d88903cdc5de194d873811ebe3
Yavuz Shafiq <bestmania107@gmail.com>
2025-01-27 21:41:01 +0000

    Changed the code of Show element from above='lg' --> when={{ base: false,sm: false, md:false, lg: true }}

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 
-import { Button } from '@chakra-ui/react'
+
 
 function App() {
   
 
   return (
     
-      <Button colorScheme="light">Hola!</Button>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area="nav" bg="cyan.500">
+        Nav
+      </GridItem>
+      <Show  when={{ base: false,sm: false, md: false, lg: true }}> // Changed code here
+      <GridItem area="aside" bg="skyblue">
+        Aside
+      </GridItem>
+      </Show>
+      <GridItem area="main" bg="crimson">
+        Main
+      </GridItem>
+    </Grid>
     
   )
 }
